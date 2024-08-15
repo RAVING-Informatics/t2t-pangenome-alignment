f6c7f1a9cdfcaf1bb126daa94398ec7b18196a9f
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2024-08-15 14:03:58 +0800

    Create pangenie.sh
    Script to run pangenie

---
+++ Scripts-slurm/pangenie.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=pangenie
+#SBATCH --account=pawsey0933
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=chiara.folland@perkins.org.au
+#SBATCH --mail-type=END
+#SBATCH --error=%j.%x.err
+#SBATCH --output=%j.%x.out
+#SBATCH --export=ALL
+
+# Load modules
+module load singularity/4.1.0-slurm
+module load bcftools/1.15--haf5b3da_0
+module load samtools/1.15--h3843a85_0 #htslib
+
+# Set variables 
+input_file=/scratch/pawsey0933/cfolland/pangenie/samplesheet_test.csv
+image_dir=/scratch/pawsey0933/cfolland/pangenie/container/
+image_name=/scratch/pawsey0933/cfolland/pangenie/container/pangenie.sif
+ref=/scratch/pawsey0933/cfolland/pangenie/refs/CHM13v11Y.fa
+graph_vcf=/scratch/pawsey0933/cfolland/pangenie/refs/chm13_cactus_filtered_ids_biallelic.vcf
+outdir=/scratch/pawsey0933/cfolland/pangenie/output
+decomp_script=/scratch/pawsey0933/cfolland/pangenie/scripts_gs/convert-to-biallelic.py
+
+# Read the input file and process each sample
+awk -F, 'NR>1 {print $1, $2, $3}' $input_file | while read -r sample fq1 fq2; do
+    echo "Processing sample: $sample"
+
+    # Run PanGenie
+    singularity exec -B /scratch/pawsey0933/cfolland/pangenie ${image_name} PanGenie \
+        -i <(zcat ${fq1} ${fq2}) \
+        -r ${ref} \
+        -v ${graph_vcf} \
+        -t 23 \
+        -j 23 \
+        -o ${outdir}/${sample}
+
+    # Decompose bubbles
+    cat ${outdir}/${sample}_genotyping.vcf | python3 ${decomp_script} ${graph_vcf} \
+        > ${outdir}/${sample}_genotyping_biallelic.vcf
+
+    # Collect vcf stats
+    bcftools stats ${outdir}/${sample}_genotyping_biallelic.vcf > ${outdir}/${sample}_genotyping_biallelic.stats
+
+done
