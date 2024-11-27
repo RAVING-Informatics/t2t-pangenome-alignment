c0e1aae32ea0d84d5b1d93c8a2b36eaca2c1cddb
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2024-11-27 15:44:07 +0800

    Update filter_qc.sh

--- Scripts/postprocessing/filter_qc.sh
+++ Scripts/postprocessing/filter_qc.sh
@@ -1,5 +1,3 @@
-#Use this script to filter out low quality variants
-
 #!/bin/bash -l
 
 #SBATCH --job-name=filter_qc
@@ -22,11 +20,21 @@ module load bcftools/1.15--haf5b3da_0
 conda activate vcf_tools
 
 cd /scratch/pawsey0933/cfolland/t2t/
+ref=/software/projects/pawsey0933/t2t/chm13v2.0.maskedY.rCRS.EBV.fasta
+
+#convert from multiallelic to bi-allelic
+bcftools norm -m -any --check-ref skip -f $ref -o sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz sorted_toref_genmod_final_hprc_gnomad_1000g.vcf.gz
+tabix -p vcf sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz
 
 #Filter based on qc 
-bcftools view -v snps,indels sorted_toref_genmod_final_hprc_gnomad_1000g.vcf | bcftools filter -e 'ID~"^SV" || ID~"^Manta"' | bcftools view -i 'FILTER="PASS" && QUAL > 10' -o sorted_toref_genmod_final_snps_qc.vcf.gz
-bcftools view -v other,indels sorted_toref_genmod_final_hprc_gnomad_1000g.vcf | bcftools filter -e 'ID~"^chr"' | bcftools view -i 'FILTER="PASS" && QUAL > 250' -o sorted_toref_genmod_final_manta_qc.vcf.gz
-bcftools view -v other,indels sorted_toref_genmod_final_hprc_gnomad_1000g.vcf | bcftools filter -e 'ID~"^chr" || ID~"^Manta"' | bcftools view -i 'FILTER="PASS" && QUAL > 10' -o sorted_toref_genmod_final_tiddit_qc.vcf.gz
+bcftools view -v snps,indels sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz | bcftools filter -e 'ID~"^SV" || ID~"^Manta"' | bcftools view -i 'FILTER="PASS" && QUAL > 10' -Oz -o sorted_toref_genmod_final_snps_qc.vcf.gz
+bcftools view -v other,indels sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz | bcftools filter -e 'ID~"^chr"' | bcftools view -i 'FILTER="PASS" && QUAL > 250' -Oz -o sorted_toref_genmod_final_manta_qc.vcf.gz
+bcftools view -v other,indels sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz | bcftools filter -e 'ID~"^chr" || ID~"^Manta"' | bcftools view -i 'FILTER="PASS" && QUAL > 10' -Oz -o sorted_toref_genmod_final_tiddit_qc.vcf.gz
+
+##Filter based on qc 
+bcftools view -v snps,indels sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz | bcftools filter -e 'ID~"^SV" || ID~"^Manta"'  -Oz -o sorted_toref_genmod_final_snps.vcf.gz
+bcftools view -v other,indels sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz | bcftools filter -e 'ID~"^chr"' -Oz -o sorted_toref_genmod_final_manta.vcf.gz
+bcftools view -v other,indels sorted_toref_genmod_final_hprc_gnomad_1000g_biallelic.vcf.gz | bcftools filter -e 'ID~"^chr" || ID~"^Manta"' -Oz -o sorted_toref_genmod_final_tiddit.vcf.gz
 
 #tabix
 tabix -p vcf sorted_toref_genmod_final_snps_qc.vcf.gz 
