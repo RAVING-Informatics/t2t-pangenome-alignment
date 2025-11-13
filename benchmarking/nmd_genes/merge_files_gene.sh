42898b4907d54cf4c4f446c80f18dd18f8868925
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2025-11-13 10:51:47 +0800

    Create merge_files_gene.sh

---
+++ benchmarking/nmd_genes/merge_files_gene.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --job-name=merge
+#SBATCH --account=pawsey0933
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --time=0:40:00
+#SBATCH --mail-user=chiara.folland@perkins.org.au
+#SBATCH --mail-type=END
+#SBATCH --error=%j.%x.err
+#SBATCH --output=%j.%x.out
+#SBATCH --export=ALL
+
+#single gene/interval
+gene='ABCA7'
+interval="chr19:1040106-1065572"
+single_py=/software/projects/pawsey0933/benchmarking/nmd_genes/collect_coverage_perbase.py
+
+#mutliple genes/intervals
+bed_py=/software/projects/pawsey0933/benchmarking/nmd_genes/collect_coverage_perbase_bed.py
+bed=/software/projects/pawsey0933/benchmarking/nmd_genes/nmd_gene_list_grch38.bed
+merged=all.perbase_mosdepth_${method}_${ref}.tsv
+
+#shared
+ref=hg38
+method=linear
+
+cd /scratch/pawsey0933/cfolland/mosdepth/results/$method/$ref
+
+module load samtools/1.15--h3843a85_0
+source /software/projects/pawsey0933/cfolland/miniconda3/etc/profile.d/mamba.sh
+
+#single gene/interval
+python3 $single_py $gene $interval *.$ref.$method.per-base.bed.gz --index --threads 8 --end-inclusive -o $gene.perbase_mosdepth_${method}_${ref}.tsv
+
+#bed file witrh multiple genes/intervals
+python3 $bed_py $bed *.$ref.$method.per-base.bed.gz --index --threads 8 --bed-end-inclusive -o $merged
