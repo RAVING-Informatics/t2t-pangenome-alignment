d0b88d27ce2e996fab7f55ae66c37fa55484d1ec
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2025-12-16 16:05:29 +0800

    Update run_sarek_chm13.sh

--- sarek/run_sarek_chm13.sh
+++ sarek/run_sarek_chm13.sh
@@ -2,32 +2,38 @@
 
 #SBATCH --nodes=1
 #SBATCH -c 2
-#SBATCH --job-name=sarek_align
+#SBATCH --job-name=sarek_bwamem2
 #SBATCH --partition=work
 #SBATCH --account=pawsey0933
 #SBATCH --mem=20gb
 #SBATCH --time=1-00:00:00
 #SBATCH --export=NONE
-#SBATCH --mail-user=gavin.monahan@perkins.org.au
+#SBATCH --mail-user=chiara.folland@perkins.org.au
 #SBATCH --mail-type=END
 #SBATCH --error=%j.%x.err
 #SBATCH --output=%j.%x.out
 
-export NXF_WORK=/scratch/pawsey0933/T2T
+export NXF_WORK=/scratch/pawsey0933/cfolland/t2t/nfcore
+export SINGULARITY_CACHEDIR=/scratch/pawsey0933/cfolland/.singularity
 export NXF_OPTS='-Xms1g -Xmx19g'
 
-module load singularity/3.11.4-slurm
-module load nextflow/23.10.0
+module load singularity/4.1.0-slurm
+module load nextflow/24.10.0
 
 nextflow run nf-core/sarek \
+	-r 3.4.0 \
 	-profile singularity,pawsey_setonix \
-	-resume \
-	--input /software/projects/pawsey0933/gmonahan/T2T/sarek_samplesheet.csv \
-	--aligner dragmap \
-	--save_mapped \
-	--genome null \
-	--skip_tools baserecalibrator \
-	--igenomes_ignore \
-	--fasta /scratch/pawsey0933/T2T/reference/chm13v2.0.maskedY.rCRS.EBV.fasta \
-	--fasta_fai /scratch/pawsey0933/T2T/reference/chm13v2.0.maskedY.rCRS.EBV.fasta.fai \
-	--outdir /scratch/pawsey0933/T2T/sarek_align
+	-config /software/projects/pawsey0933/t2t/sarek/pawsey_setonix.config \
+    -resume \
+    --input /software/projects/pawsey0933/t2t/sarek/samplesheet_batch_garvan.csv \
+    --aligner bwa-mem2 \
+    --save_mapped \
+    --genome null \
+    --skip_tools markduplicates,baserecalibrator,vcftools \
+    --igenomes_ignore \
+	--tools deepvariant,manta,tiddit,cnvkit \
+	--fasta /software/projects/pawsey0933/pangenome/refs/chm13v2.0.maskedY.rCRS.EBV.fasta  \
+	--fasta_fai /software/projects/pawsey0933/pangenome/refs/chm13v2.0.maskedY.rCRS.EBV.fasta.fai \
+	--bwamem2 /scratch/pawsey0933/cfolland/t2t/nfcore/57/acfde7c69ad0f4b5be0889b1767d8d/bwamem2 \
+	--save_reference \
+	--outdir /scratch/pawsey0933/cfolland/t2t/nfcore/sarek_bwamem2
