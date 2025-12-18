412a3541771cac5d946fa2be0893716332349344
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2025-12-18 11:45:39 +0800

    Update and rename merge_files_nmd.sh to merge_regions_nmd.sh

--- benchmarking/nmd_genes/merge_regions_nmd.sh
+++ benchmarking/nmd_genes/merge_regions_nmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=merge
+#SBATCH --job-name=merge_regions_nmd
 #SBATCH --account=pawsey0933
 #SBATCH --partition=work
 #SBATCH --ntasks=1
@@ -21,4 +21,4 @@ cd /scratch/pawsey0933/cfolland/mosdepth/results/$mode
 module load samtools/1.15--h3843a85_0
 source /software/projects/pawsey0933/cfolland/miniconda3/etc/profile.d/mamba.sh
 
-python3 collect_coverage.py *.$ref.$mode.regions.bed.gz -o nmd.$ref.$mode.merged.tsv
+python3 collect_coverage_mosdepth_regions.py *.$ref.$mode.regions.bed.gz -o nmd.$ref.$mode.merged.tsv
