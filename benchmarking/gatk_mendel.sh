2beebe1d9a2421e80d23865146bd72b171572d19
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2026-01-06 11:25:47 +0800

    Update gatk_mendel.sh

--- benchmarking/gatk_mendel.sh
+++ benchmarking/gatk_mendel.sh
@@ -19,9 +19,9 @@ sif=/software/projects/pawsey0933/benchmarking/mendel/gatk_latest.sif
 #ref=/software/projects/pawsey0933/sv/references/hg38_masked/Homo_sapiens_assembly38_masked.fasta
 ref=/software/projects/pawsey0933/pangenome/refs/chm13v2.0.maskedY.rCRS.EBV.fasta
 #input=/scratch/pawsey0933/cfolland/vep/annotation/glnexus/merged-dv/dv_glnexus_VEP.ann.vcf.gz
-#input=/scratch/pawsey0933/cfolland/benchmark/vcfs/deepvariant/linear/cohort/T2T_dv_glnexus_VEP.ann.vcf.gz
-input=/scratch/pawsey0933/cfolland/benchmark/vcfs/deepvariant/${approach}/cohort/${prefix}_dv_glnexus_VEP.gff.exons.vcf.gz 
-output=/scratch/pawsey0933/cfolland/benchmark/mendel_viol/${approach}/$prefix.MVs.exons.byFamily.table
+input=/scratch/pawsey0933/cfolland/benchmark/vcfs/deepvariant/linear/cohort/T2T_dv_glnexus_VEP.ann.vcf.gz
+#input=/scratch/pawsey0933/cfolland/benchmark/vcfs/deepvariant/${approach}/cohort/${prefix}_dv_glnexus_VEP.gff.exons.vcf.gz 
+output=/scratch/pawsey0933/cfolland/benchmark/mendel_viol/${approach}/$prefix.MVs.byFamily.table
 ped=/software/projects/pawsey0933/pangenome/genmod/trios_trios.ped
 
 module load singularity/4.1.0-slurm
