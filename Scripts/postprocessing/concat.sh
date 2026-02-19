9cc1d7e119c2a814598607fd317240d5c298c13e
Chiara Folland <58469884+ChiaraF32@users.noreply.github.com>
2026-02-19 10:21:00 +0800

    Update concat.sh

--- Scripts/postprocessing/concat.sh
+++ Scripts/postprocessing/concat.sh
@@ -19,16 +19,16 @@ module load bcftools/1.15--haf5b3da_0
 # Define variables
 ref=chm13
 #ref=grch38
-dv=/scratch/pawsey0933/cfolland/benchmark/vcfs/deepvariant/linear/cohort/T2T_dv_glnexus_VEP.ann.vcf.gz
+dv=/scratch/pawsey0933/cfolland/t2t/chm13/linear-chm13_glnexus_dv_VEP.ann.vcf.gz
 #dv=/scratch/pawsey0933/cfolland/vep/annotation/glnexus/merged-dv/dv_glnexus_VEP.ann.vcf.gz
-dysgu=/scratch/pawsey0933/cfolland/benchmark/vcfs/dysgu/linear/dysgu_merge_T2T_VEP.ann.vcf.gz
+dysgu=/scratch/pawsey0933/cfolland/t2t/chm13/linear/batch2_linear_chm13_dysgu_merge_sorted_VEP.ann.vcf.gz
 #dysgu=/scratch/pawsey0933/cfolland/benchmark/vcfs/dysgu/linear/dysgu_merge_hg38_VEP.ann.vcf.gz
-outdir=/scratch/pawsey0933/cfolland/t2t/vcfs/
+outdir=/scratch/pawsey0933/cfolland/t2t/chm13/
 
 # Concatenate, sort and index
-bcftools concat -a -Oz --threads 12 -o $outdir/linear_${ref}_dv_dysgu_VEP_concat.vcf.gz $dv $dysgu
+bcftools concat -a -Oz --threads 12 -o $outdir/batch2_linear_${ref}_dv_dysgu_VEP.vcf.gz $dv $dysgu
 #bcftools index -t $outdir/hprc-v1.1-mc-${ref}_dv_dysgu_VEP_concat.vcf.gz
-bcftools sort -T $outdir -o $outdir/linear_${ref}_dv_dysgu_VEP_sorted.vcf.gz $outdir/linear_${ref}_dv_dysgu_VEP_concat.vcf.gz 
+bcftools sort -T $outdir -o $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.vcf.gz $outdir/batch2_linear_${ref}_dv_dysgu_VEP.vcf.gz
 #bcftools index -t $outdir/hprc-v1.1-mc-${ref}_dv_dysgu_VEP_sorted.vcf.gz
-bcftools view -e 'CSQ[*]="."' $outdir/linear_${ref}_dv_dysgu_VEP_sorted.vcf.gz -Oz -o $outdir/linear_${ref}_dv_dysgu_VEP_sorted_noCSQ.vcf.gz
-bcftools index -t $outdir/linear_${ref}_dv_dysgu_VEP_sorted_noCSQ.vcf.gz
+bcftools view -e 'CSQ[*]="."' $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.vcf.gz -Oz -o $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.noCSQ.vcf.gz
+bcftools index -t $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.noCSQ.vcf.gz
