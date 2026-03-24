f4c02a449c1b204b8d341896ac311f38d3ce2ab3
Chiara Folland <cfolland32@gmail.com>
2026-03-24 12:40:56 +0800

    modify concat.sh

--- postprocessing/combine/concat.sh
+++ postprocessing/combine/concat.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=work
 #SBATCH --account=pawsey0933
 #SBATCH --mem=16G
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mail-user=chiara.folland@perkins.org.au
 #SBATCH --mail-type=END
 #SBATCH --error=%j.%x.err
@@ -17,18 +17,15 @@
 module load bcftools/1.15--haf5b3da_0
 
 # Define variables
-ref=chm13
-#ref=grch38
-dv=/scratch/pawsey0933/cfolland/t2t/chm13/linear-chm13_glnexus_dv_VEP.ann.vcf.gz
-#dv=/scratch/pawsey0933/cfolland/vep/annotation/glnexus/merged-dv/dv_glnexus_VEP.ann.vcf.gz
-dysgu=/scratch/pawsey0933/cfolland/t2t/chm13/linear/batch2_linear_chm13_dysgu_merge_sorted_VEP.ann.vcf.gz
-#dysgu=/scratch/pawsey0933/cfolland/benchmark/vcfs/dysgu/linear/dysgu_merge_hg38_VEP.ann.vcf.gz
-outdir=/scratch/pawsey0933/cfolland/t2t/chm13/
+ref=grch38
+dv=/scratch/pawsey0933/cfolland/t2t/batch2/grch38/dv/linear-${ref}_glnexus_dv_VEP.ann.sorted.vcf.gz
+dysgu=/scratch/pawsey0933/cfolland/t2t/batch2/grch38/dysgu/batch2_linear_hg38_dysgu_merge_sorted_VEP.ann.vcf.gz
+outdir=/scratch/pawsey0933/cfolland/t2t/batch2/${ref}/
 
 # Concatenate, sort and index
 bcftools concat -a -Oz --threads 12 -o $outdir/batch2_linear_${ref}_dv_dysgu_VEP.vcf.gz $dv $dysgu
-#bcftools index -t $outdir/hprc-v1.1-mc-${ref}_dv_dysgu_VEP_concat.vcf.gz
+bcftools index -t $outdir/batch2_linear_${ref}_dv_dysgu_VEP_concat.vcf.gz
 bcftools sort -T $outdir -o $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.vcf.gz $outdir/batch2_linear_${ref}_dv_dysgu_VEP.vcf.gz
-#bcftools index -t $outdir/hprc-v1.1-mc-${ref}_dv_dysgu_VEP_sorted.vcf.gz
+bcftools index -t $outdir/batch2_linear_${ref}_dv_dysgu_VEP_sorted.vcf.gz
 bcftools view -e 'CSQ[*]="."' $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.vcf.gz -Oz -o $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.noCSQ.vcf.gz
 bcftools index -t $outdir/batch2_linear_${ref}_dv_dysgu_VEP.sorted.noCSQ.vcf.gz
